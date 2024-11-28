7eba75995fb64cd9319843d975b910d8afc6bc89
서재원 <cinos81@gmail.com>
2024-11-28 11:31:49 +0900

    커스텀 파일 업로더 구현을 대부분 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,78 @@
 import { Select, Tag } from "antd";
 import { useForm, Controller, FormProvider } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
+import { FC, MouseEventHandler, PropsWithChildren, ReactNode } from "react";
 
-const tagRender = (props) => {
-  const { label, value, closable, onClose } = props;
-  const onMouseDown = (event) => {
-    event.preventDefault();
-    event.stopPropagation();
+type FileTagProps = {
+  label: ReactNode;
+  value: string;
+  disabled: boolean;
+  onClose: (event?: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+  closable: boolean;
+  isMaxTag: boolean;
+};
+
+const tagRender = ({ onClose }:{onClose: (value: string) => void}) => ({ label, value }: FileTagProps) => {
+  const onMouseDown: MouseEventHandler<HTMLSpanElement> = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
   };
+
   return (
     <Tag
-      color={value}
       onMouseDown={onMouseDown}
-      closable={closable}
-      onClose={onClose}
-      style={{
-        marginInlineEnd: 4,
-      }}
+      closable={true}
+      onClose={() =>onClose(value)}
     >
       {label}
     </Tag>
   );
 };
 
+const FileSelectButton: FC<PropsWithChildren<{htmlFor: string}>> = ({ children, htmlFor }) => {
+  return <label htmlFor={htmlFor}>{children}</label>
+}
+
+const makeFileID = (file: File) => `${file.name}_${file.lastModified}`
+
 const RHFileUploader = () => {
   return (
     <Controller name="test" render={({ field }) => {
       const dataTransfer = new DataTransfer()
-      const files = (field.value || []) as File[]
-      files.forEach(file => dataTransfer.items.add(file));
+      const fileList = (field.value || new DataTransfer().files) as FileList
+      Array.from(fileList).forEach(file => dataTransfer.items.add(file));
 
-      const options = files.map(file => {
+      const options = Array.from(fileList).map(file => {
         return {
-          value: file.name
+          label: file.name,
+          value: makeFileID(file),
         }
       })
 
       return (
         <>
-          <Select style={{ width: 200 }} onBlur={field.onBlur} value={options} options={options} mode="multiple" placeholder="선택하시요" tagRender={tagRender} />
-          <label htmlFor="hello">
+          <Select 
+            style={{ width: 200 }} 
+            onBlur={field.onBlur} 
+            value={options} 
+            options={options} 
+            mode="multiple" 
+            placeholder="선택하시요" 
+            tagRender={tagRender({ onClose: (value) => {
+              const index = Array.from(fileList).findIndex(file => makeFileID(file) === value)
+              if (index >= 0) {
+                dataTransfer.items.remove(index)
+                field.onChange(dataTransfer.files)
+              }
+            } })} 
+          />
+          <FileSelectButton htmlFor="hello">
             파일 선택
-          </label>
+          </FileSelectButton>
           <input style={{ display: 'none' }} type="file" id="hello" name={field.name} onChange={(e) => {
             dataTransfer.items.clear()
             Array.from(e.currentTarget.files|| []).forEach(file => dataTransfer.items.add(file));
-            field.onChange(Array.from(dataTransfer.files));
+            field.onChange(dataTransfer.files);
           }} onBlur={field.onBlur} ref={field.ref} multiple={true} />
         </>
       )
